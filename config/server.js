0dbd7afddcfd459b302a9ea832e497e37f6993e9
tatangharyadi <tatang.haryadi@gmail.com>
2021-07-08 17:01:36 +0700

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c26648af01ef82f515fa0b20d8376b03'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2098a5da-d99b-4850-9b81-61be004f0d24'
+};
